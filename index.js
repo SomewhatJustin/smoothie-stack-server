d01656341db8390fd684225935bef46b77e76842
Justin Fowler <justin.fowler@hey.com>
2022-07-15 10:53:49 -0500

    more cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 const app = express()
 app.use(express.json())
 app.use(cors({
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
   origin: ['https://mysmoothiestack.com', 'http://localhost:3000']
 }));
 
